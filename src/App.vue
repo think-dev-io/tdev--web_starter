ae7644cb75d69d834c1d34f087556e73c053e083
BIYA Paul <paul@workerly.io>
2022-09-17 02:45:26 +0100

    feat: Update ui.

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,12 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Think-dev Web starter',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'Opinionated Vite Starter Template for think-dev web applications' },
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
-    },
-  ],
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
     },
   ],
 })
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,14 +13,6 @@ const { t } = useI18n()
 
 <template>
   <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
     <p>
       <em text-sm opacity-75>{{ t('intro.desc') }}</em>
     </p>
